d827258dd9811c8db4a70a00dbdb5b0bb2c5a247
Nehat DeDinca <nehatdedinca007@gmail.com>
2018-02-20 20:59:35 +0800

    Initial frontend design

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
+// Listen for form Submit
+document.getElementById('myForm').addEventListener('submit', calculateZodiacSign);
 
-function GetSelectedValue(e){
-  var valueSelected = document.getElementById('option').value;
-  alert(valueSelected);
-  console.log(valueSelected);
+
+function calculateZodiacSign(e){
+  // Get values of Name and Age
+  var name = document.getElementById('nameInput').value;
+  var age = document.getElementById('age').value;
+
+  // Alert the result for testing purpose
+  alert('Name: ' + name + ' Age: ' + age + '!')
 
   // Prevent form from submitting
   e.preventDefault();
+}
+
+// Get the initial age slider value
+function getInitialAgeValue() {
+  var slider = document.getElementById("age");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value;
+}
+
+// Display the age value dynamically
+function displayYearOld() {
+  var slider = document.getElementById("age");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value;
+
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
 }
